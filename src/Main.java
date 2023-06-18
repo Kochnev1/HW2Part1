aefa06dfde8aec2d834d05ac60c2253df4168049
Roman Kochnev <romaslog@icloud.com>
2023-06-18 21:50:15 +0500

    Result

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 97250; //ЦенаБилета
+        int rub = 20;// КоличествоРублейДляБонуснойМили
+        System.out.println("Количество бонусных миль:");
+        System.out.println(price / rub);
+
+
+    }
 }
